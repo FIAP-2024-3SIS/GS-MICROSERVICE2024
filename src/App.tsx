e5bead5d6eaff48820140d07a61598ab3c7220de
GuilhermeMViana <gmunizviana@gmail.com>
2024-06-05 20:11:02 -0300

    wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
+import { useEffect } from "react";
+import { DataTable, FiltersContainner, Header, SelectFilters } from "./styles";
+
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    fetch(`https://fiap-3sis-gs-20241.azurewebsites.net`)
+      .then((response) => response.json())
+      .then((data) => {
+        const apiData = data.data;
+        console.log(apiData);
+      });
+  }
 
   return (
     <>
-      <h1>Hello world!</h1>
+      <Header>
+        <a href="">Oceanic Explorer</a>
+        <a href="">Home</a>
+        <a href="">Ocean Conditions</a>
+        <a href="">Endagered Conservation</a>
+        <a href="">Projects</a>
+      </Header>
+      <FiltersContainner>
+        <h2>Filters</h2>
+        <SelectFilters>
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </SelectFilters>
+        <div>
+          <button>Search</button>
+        </div>
+      </FiltersContainner>
+      <DataTable>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          {/* {data.length > 0 && data.map( (item) => (
+            <tbody>
+              <tr>
+                <td>{item.}</td>
+                <td>{item.}</td>
+                <td>{item.}</td>
+                <td>{item.}</td>
+              </tr>
+            </tbody>
+          ))     
+          } */}
+        </table>
+      </DataTable>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+    display: flex;
+    flex-direction: row;
+    background-color: #22d3ee;
+    width: 100%;
+    height: 50px;
+    justify-content: space-around;
+    align-items: center;
+
+`
+
+export const FiltersContainner = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+    margin-top: 50px;
+    padding: 10px;
+
+    border: 1px solid black;
+
+    h2 {
+        margin-left: 110px
+    }
+
+    button {
+        margin-left: 110px
+    }
+`
+
+export const SelectFilters = styled.div`
+ display: flex;
+ flex-direction: row;
+ margin: 0 auto;
+ gap: 5px;
+`
+
+export const DataTable = styled.div`
+    
+`
